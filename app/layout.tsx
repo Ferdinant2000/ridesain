6849ac72f3cca6916da44ad943dab4be775fb5ba
Ferdinant2000 <firdavskomolitdinov@gmail.com>
2025-08-14 13:38:19 +0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { HeroUIProvider } from "@heroui/react"
+import '../styles/globals.css'
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <HeroUIProvider>
+          {children}
+        </HeroUIProvider>
+      </body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import Navbar from "@/components/Navbar/Navbar"
+import News from "@/components/News/News"
+import Collection from "@/components/Collection/Collection"
+import Decorate from "@/components/Decorate/Decorate"
+import Assortment from "@/components/Assortment/Assortment"
+import Works from "@/components/Works/Works"
+import Join from "@/components/Join/Join"
+import Footer from "@/components/Footer/Footer"
+
+export default function HomePage() {
+  return (
+    <>
+      {/* <Navbar />
+      <News />
+      <Collection />
+      <Decorate />
+      <Assortment />
+      <Works />
+      <Join />
+      <Footer /> */}
+    </>
+  )
+}
