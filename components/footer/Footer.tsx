994c84da6c744da8b4b7cf1ad6213d3b58e8e747
Ferdinant2000 <firdavskomolitdinov@gmail.com>
2025-08-24 21:09:12 +0500

    Added section 'Beauty Care' (/Footer)

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,3 +1,24 @@
+import styles from "./Footer.module.css";
+
 export default function Footer() {
-  return <div>Footer placeholder</div>;
-}
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerLeft}>
+        <h2 className={styles.footerTitle}>Beauty Care</h2>
+        <p className={styles.footerText}>
+          Do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.
+        </p>
+        <h3 className={styles.footerSubtitle}>Follow Us</h3>
+      </div>
+      <div className={styles.footerRight}>
+        <h2 className={styles.footerTitle}>Instagram Shop</h2>
+        <div className={styles.footerImages}>
+          <img src="/images/footer-images/image-94.png" alt="" className={styles.footerImg} />
+          <img src="/images/footer-images/image-95.png" alt="" className={styles.footerImg} />
+          <img src="/images/footer-images/image-96.png" alt="" className={styles.footerImg} />
+          <img src="/images/footer-images/image-98.png" alt="" className={styles.footerImg} />
+        </div>
+      </div>
+    </footer>
+  );
+};
